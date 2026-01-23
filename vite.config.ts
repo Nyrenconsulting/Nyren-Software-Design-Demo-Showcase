3a12232daacbbbf9b8d83e2b83c2c851516e92ba
Nyrenconsulting <max@nyrenconsulting.se>
2026-01-23 18:41:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // behövs fortfarande
+  base: "./", // behövs fortfarande
   plugins: [react()],
   test: {
     environment: "jsdom",
